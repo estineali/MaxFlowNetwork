053d149b8af4a2b2d52157add0483a6f54f9d64f
Muhammad Shahrom Ali <estineali@gmail.com>
2019-12-02 02:25:57 +0500

    n-order Graph generation

---
+++ code/GraphGeneration.py
@@ -0,0 +1,21 @@
+import random
+
+def GenerateGraph(lengthG):
+    #Generate adjacency matrix of size lengthG x lengthG 
+
+    G = [[0 for i in range(lengthG)] for j in range(lengthG)]
+    for i in range(lengthG):
+        for j in range(len(G[i])):
+            capacity = random.randint(-20, 15)
+            if capacity > 0 and j != i:
+                G[i][j] = capacity
+    return G
+
+def showGraph(G):
+    for i in range(len(G)):
+        print(i, ":", G[i])
+
+size = 6 #change this 
+
+capacity = GenerateGraph(size)
+showGraph(capacity)
