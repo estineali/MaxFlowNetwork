d688cefd7d59d20ef0f3925a7023ca156473f3b7
Muhammad Shahrom Ali <estineali@gmail.com>
2019-12-02 02:26:22 +0500

    Update GraphGeneration.py

--- code/GraphGeneration.py
+++ code/GraphGeneration.py
@@ -2,7 +2,7 @@ import random
 
 def GenerateGraph(lengthG):
     #Generate adjacency matrix of size lengthG x lengthG 
-
+    random.seed(100)
     G = [[0 for i in range(lengthG)] for j in range(lengthG)]
     for i in range(lengthG):
         for j in range(len(G[i])):
